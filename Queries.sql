28b1bc178c37719734b2736429df33d6515c9f32
Diego Santos <120533129+madax14@users.noreply.github.com>
2024-08-25 21:52:11 -0300

    Update Queries.sql
    Added Medals by country

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,18 @@
+# How many athletes participated of the olympics
+SELECT
+COUNT(DISTINCT code)
+FROM
+  `paris-2024-summer-olympic.2024_Olympic.Athletes`
+
+# Medals per country
+SELECT
+country_code,
+COUNT(CASE WHEN `medal_type` = "Gold Medal" THEN 1 END) AS `Gold`,
+COUNT(CASE WHEN `medal_type` = "Silver Medal" THEN 1 END) AS `Silver`,
+COUNT(CASE WHEN `medal_type` = "Bronze Medal" THEN 1 END) AS `Bronze`,
+COUNT(medal_type) `Medals`
+FROM
+  `paris-2024-summer-olympic.2024_Olympic.Medals`
+GROUP BY country_code
+ORDER BY `Medals` DESC 
 
