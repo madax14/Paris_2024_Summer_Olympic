5831f2a36778b9617bd6ec7324efb695d6ce280e
Diego Santos <120533129+madax14@users.noreply.github.com>
2024-09-04 21:54:48 -0300

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -16,3 +16,15 @@ FROM
 GROUP BY country_code
 ORDER BY `Medals` DESC 
 
+#Percentage gender winners
+SELECT
+gender,
+ROUND((COUNT(medal_type) / (SELECT COUNT(*) FROM `2024_Olympic.Medals`)*100),2) AS `percentage`
+FROM `2024_Olympic.Medals`
+GROUP BY gender
+ORDER BY percentage DESC+
+
+#String to Integer
+UPDATE
+  `paris-2024-summer-olympic.2024_Olympic.Medals`
+SET code = CAST(code AS INT64)
